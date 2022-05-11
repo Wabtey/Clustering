6c6832148b40ef8e34f86bde1a79fc3e8097b01d
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-05-11 16:28:09 +0200

    step 3 of clusterize

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod sequence;
 use sequence::*;
 
 fn main() {
-    let seq_1 = Sequence::new_string("ATTACG".to_string());
-    let seq_2 = Sequence::new_string("ATATCG".to_string());
-    let seq_3 = Sequence::new_string("ACCCCG".to_string());
-    let seq_4 = Sequence::new_string("GGGGAA".to_string());
-    let seq_5 = Sequence::new_string("TTTACG".to_string());
-    let seq_6 = Sequence::new_string("ATTAC".to_string());
-    let seq_7 = Sequence::new_string("ATATC".to_string());
+    let seq_1 = Sequence::new_with_string("ATTACG".to_string());
+    let seq_2 = Sequence::new_with_string("ATATCG".to_string());
+    let seq_3 = Sequence::new_with_string("ACCCCG".to_string());
+    let seq_4 = Sequence::new_with_string("GGGGAA".to_string());
+    let seq_5 = Sequence::new_with_string("TTTACG".to_string());
+    let seq_6 = Sequence::new_with_string("ATTAC".to_string());
+    let seq_7 = Sequence::new_with_string("ATATC".to_string());
 
     println!("{}", seq_1.levenshtein_distance(&seq_1));
     println!("{}", seq_1.levenshtein_distance(&seq_2));
@@ -19,7 +19,13 @@ fn main() {
     println!("{}", seq_1.levenshtein_distance(&seq_3));
     println!("{}", seq_2.levenshtein_distance(&seq_3));
     println!("{}", seq_1.levenshtein_distance(&seq_4));
-}
+
+    let sequences = vec![seq_1, seq_2, seq_3, seq_4, seq_5];
+    let bio_cluster = ClusterOfSequence::new_with_sequences(sequences);
+    println!("bio_cluster {}", bio_cluster.get_newick());
+    // bio_cluster.clusterize();
+    // println!("bio_cluster {}", bio_cluster.get_newick());
+} 
 
 /*
  * human same hemoglobine :
--- src/sequence.rs
+++ src/sequence.rs
@@ -1,4 +1,4 @@
-use std::{io::prelude::*, str::*};
+use std::{io::prelude::*, str::*, vec::*};
 
 #[derive(Clone)]
 pub struct Sequence {
@@ -13,13 +13,13 @@ impl Sequence {
         }
     }
 
-    pub fn new_string(s: String) -> Sequence { 
+    pub fn new_with_string(s: String) -> Sequence { 
         Sequence {
             seq: s,
         }
     }
 
-    pub fn new_sequecence(s: Sequence) -> Sequence { 
+    pub fn new_with_sequence(s: Sequence) -> Sequence { 
         Sequence {
             seq: s.seq,
         }
@@ -82,17 +82,162 @@ impl Sequence {
 
 }
 
-struct ClusterOfSequence {
+#[derive(Clone)]
+pub struct ClusterOfSequence {
     sub_clusters: Vec<ClusterOfSequence>,
     elements: Vec<Sequence>,
 }
 
-impl ClusterOfSequence {
-    pub fn new(elements_in: Sequence) -> ClusterOfSequence{
+impl ClusterOfSequence
+{
+    pub fn new(element: Sequence) -> ClusterOfSequence
+    {
         ClusterOfSequence {
             sub_clusters: Vec::new(),
-            elements: vec![elements_in],
+            elements: vec![element],
         }
+    }
+
+    pub fn new_with_sequences(elements_in:Vec<Sequence>) -> ClusterOfSequence
+    {
+        ClusterOfSequence {
+            sub_clusters: {
+                let mut res = Vec::new();
+                for e in &elements_in {
+                    res.push(ClusterOfSequence::new(e.clone()))
+                }
+                res
+            },
+            elements: elements_in,
+        }
+    }
+
+    pub fn new_with_clusters(clusters_1: &ClusterOfSequence,
+                        clusters_2: &ClusterOfSequence) -> ClusterOfSequence
+    {
+        ClusterOfSequence {
+            sub_clusters: vec![clusters_1.clone(), clusters_2.clone()],
+            // get all seq of clust_1 and clust_2 to elements
+            elements: 
+            {
+                let mut n = clusters_1.clone().elements;
+                n.extend(clusters_2.clone().elements);
+                n
+            }
+        }
+    }
+
+    pub fn linkage(&self, a_cluster: ClusterOfSequence) -> Option<f32>
+    {
+        // covert the division by 0
+        if self.elements.len() == 0 || a_cluster.elements.len() == 0 {
+            None
+            // f32::MAX;
+        }else {
+            let mut result=0.00;
+            let mut counter = 0.00;
+            /*
+             * compare every e1: Sequence (in the self called Cluster)
+             *      with every e2: Sequence (in a_cluster)
+             *      by the levenshtein_distance
+             * return the average distance between all sequence
+             */ 
+            for e1 in &self.elements {
+                for e2 in &a_cluster.elements {
+                    result += e1.levenshtein_distance(&e2) as f32;
+                    counter += 1 as f32;
+                }
+            }
+    
+            result = result/counter;
+            Some(result)
+        }
+    }
+    pub fn get_newick(&self) -> String 
+    {
+        let mut res = String::from("(");
+        if self.sub_clusters.len() != 0 {
+            for sub in &self.sub_clusters {
+                res.push_str(sub.get_newick().as_str());
+                res.push(',');
+            }
+        }else {
+            for e in &self.elements {
+                res.push_str(e.seq.as_str());
+                res.push(',');
+            }
+        }
+        res.push(')');
+        res
+    }
+
+    /**
+     * first approch :
+     *  compare each sequence
+        group 2 close sequences in new cluster
+        group other sequences in another cluster
+        clusterize the second cluster
+        -------------------------
+        second approch :
+        calculate the distance for each sequence two by two
+        compare this distance with ALL of the other sequence
+        if the first distance is the smallest
+            create the sub-cluster containning the two sequence
+        verify if the members are coherent 
+            distance between all of them
+            is smaller than any distance of a another sequence (in the universe)
+        if this cluster is good to go, create it for good.
+        if the cluster the most coherent is composed only by one seq, create it.
+        compare two cluster 
+        -------------------------
+        third approch :
+        state 1 : create clusters for all sequence indiv (ok when initilization)
+        state 2 : compare all sub-clusters one by one by distance
+        state 2.1 :     get the smallest distance
+        state 2.2 :     check cluster condition
+                             if it's correct confirm this cluster
+                             else switch to the next
+        state 3 : back to state 2 until the total of sub-cluster =< 2 
+     */
+    pub fn clusterize_agglomerative(&mut self) 
+    {
+        while self.sub_clusters.len() > 2 {
+            'main_loop:
+            for i in 0..self.sub_clusters.len() {
+                let mut min: f32 = f32::MAX;
+                let mut keep = 0;
+                for j in 0.. self.sub_clusters.len() {
+                    if i != j {
+                        let dist = self.sub_clusters[i]
+                                       .linkage(self.sub_clusters[j].clone())
+                                       .unwrap();
+                        if dist < min {
+                            min = dist;
+                            keep = j;
+                        }
+                    }
+                }
+                for j in 0..self.sub_clusters.len() {
+                    if j != keep && j != i {
+                        if self.sub_clusters[keep]
+                               .linkage(self.sub_clusters[j].clone())
+                               .unwrap() < min {
+                            continue 'main_loop;
+                        }
+                    }
+                }
+                // ALED
+            }
+        }
+    }
+
+    /**
+     * reform the cluster structure of self
+     * @param himself
+     * @param n, the number of sub division needed
+     */
+    pub fn clusterize_divisive(&self, n: i32) {
+
     }
 }
 
